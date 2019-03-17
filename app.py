916fb6d4138f1cebc39f12a28e921df4a33b0b92
anjithadivakar <anjithadivakar@atlassian.com>
2019-03-17 14:57:44 +0530

    Modification in the sidabar navigation has been made by adding 2 sub menu for profile menu ie. view profile and edit profile and a page for settings is created and code for connecting the settings.html to database was also written in app.py file.Validation messages and codes also completed for signup and login. Display of alert messages was also done as a change in previously commited file.

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request, redirect, flash, session
 from passlib.hash import sha256_crypt
 import gc
 
-
 app = Flask(__name__)
 app.debug = True
 app.secret_key = 'some secret key'
@@ -48,18 +47,25 @@ def check_user():
         email = request.form['email']
         user_password = request.form['pass']
         cursor = connection.cursor()
-        com = "select * from login where u_email='"+email+"'"
+        com = "select * from login where u_email='" + email + "'"
         cursor.execute(com)
         data = cursor.fetchone()[2]
         cursor.close()
         if sha256_crypt.verify(user_password, data):
-                session['logged_in'] = True
-                session['username'] = email
-                return render_template("/studenthome.html")
+            session['logged_in'] = True
+            session['username'] = email
+            session['id'] = data
+            return render_template("/studenthome.html")
         else:
-                flash("Invalid Login")
+            flash("Invalid Login")
         gc.collect()
         return redirect("/login.html")
+
+
+@app.route('/editprofile.html')
+def editprofile():
+    session.pop('user', None)
+    return render_template("/editprofile.html")
 
 
 @app.route('/logout')
@@ -83,27 +89,41 @@ def quiz():
     return render_template("/question.html")
 
 
+@app.route("/settings.html")
+def settings():
+    return render_template("/settings.html")
+
+
 @app.route('/post_user', methods=['POST'])  # sign up function
 def post_user():
     if request.method == 'POST':
         cursor = connection.cursor()
         email = request.form['email']
         password = sha256_crypt.encrypt(request.form['pass'])
-        x = cursor.execute("select * from login where u_email='"+email+"'")
+        confirm= sha256_crypt.encrypt(request.form['passw'])
+        x = cursor.execute("select * from login where u_email='" + email + "'")
         if int(x) > 0:
-                flash("That username is already taken, please choose another")
-                return redirect("/signup.html")
+            flash("That username is already taken, please choose another")
+            return redirect("/signup.html")
         else:
-            sql = """ALTER TABLE login AUTO_INCREMENT = 100"""
-            cursor.execute(sql)
-            com = """insert into login (u_email,password) values (%s, %s)"""
-            cursor.execute(com, (email, password))
-            connection.commit()
-            flash("Thanks for registering!")
-            session['logged_in'] = True
-            session['username'] = email
-            cursor.close()
-            return redirect("/profile.html")
+            if request.form['pass'] == request.form['passw']:
+                sql = """ALTER TABLE login AUTO_INCREMENT = 100"""
+                cursor.execute(sql)
+                com = """insert into login (u_email,password) values (%s, %s)"""
+                cursor.execute(com, (email, password))
+                query = "select * from login where u_email='" + email + "'"
+                cursor.execute(query)
+                data = cursor.fetchone()[0]
+                connection.commit()
+                flash("Thanks for registering!")
+                session['logged_in'] = True
+                session['username'] = email
+                session['id'] = data
+                cursor.close()
+                return redirect("/profile.html")
+            else:
+                flash("Password not same")
+                return redirect("/signup.html")
 
 
 @app.route("/profile.html")
@@ -115,15 +135,61 @@ def profile():
 def post_profile():
     if request.method == 'POST':
         cursor = connection.cursor()
-        cursor.execute()
-        email = request.form['email']
-        password = request.form['pass']
-        com = """insert into login (uemail,password) values (%s, %s)"""
-        cursor.execute(com, (email, password))
+        id = session['id']
+        firstnme = request.form['frst_name']
+        lst_nme = request.form['lst_name']
+        dob = request.form['dob']
+        gender = request.form['optradio']
+        cntno = request.form['phn_no']
+        email = session['username']
+        institute = request.form['inst']
+        clas = request.form['clasnme']
+        house = request.form['house_name']
+        city = request.form['city']
+        country = request.form['country']
+        pin = request.form['pin_code']
+        # img= request.file['file']
+        com = "insert into user_profile values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        cursor.execute(com,
+                       (id, firstnme, lst_nme, dob, gender, cntno, email, institute, clas, house, city, country, pin))
         connection.commit()
-        return redirect("/profile.html")
+        return redirect("/studenthome.html")
     connection.close()
+
+
+@app.route('/change_password', methods=['POST'])  # change password
+def change_password():
+    if request.method == 'POST':
+        cursor = connection.cursor()
+        uid = session['username']
+        newpassword = sha256_crypt.encrypt(request.form['password1'])
+        com = "update login set password ='"+newpassword+"' where u_email = '"+uid+"' "
+        cursor.execute(com)
+        connection.commit()
+        flash("Password updated")
+        return redirect("/settings.html")
+
+
+@app.route('/delete_user', methods=['POST'])        # Delete Account
+def delete_user():
+    if request.method == 'POST':
+        uid = session['username']
+        cursor = connection.cursor()
+        command = "delete from user_profile where stud_email = '" + uid + "'"
+        cursor.execute(command)
+        query = "delete from login where u_email= '" + uid + "'"
+        cursor.execute(query)
+        connection.commit()
+        cursor.close()
+        session.pop('user', None)
+        return redirect("/home.html")
+
+    else:
+        flash("Wrong password")
+        return redirect("/settings.html")
 
 
 if __name__ == "__main__":
     app.run()
+
+
